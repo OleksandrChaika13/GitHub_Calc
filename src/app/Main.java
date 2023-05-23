bb91178aa3373633c49990b950636a5c70f87694
OleksandrChaika13 <axelrod1347@gmail.com>
2023-05-23 18:53:12 +0300

    Add addition

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello GitHub world! It`s feeling great!");
+
+        int a = 13;
+        int b = 5;
+        int c = add(a, b);
+        System.out.println(c);
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
     }
 }
